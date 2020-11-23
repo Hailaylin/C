f08d8fe7b31ffd2a6a1d63e9a42b54a6e7b6bffa
HailayLin <hailay@qq.com>
2020-11-23 10:19:10 +0800

    修复一下Arduino includePath的报错

--- UPRE/C_Task_6_Struct_ZhiZhen.c
+++ UPRE/C_Task_6_Struct_ZhiZhen.c
@@ -22,13 +22,15 @@ int main()
 {
 
     struct student std1={"林嘉濠","男","202030701"};
-    struct student std2={"林夕玥","女","207230701"};
+    struct student std2={"林夕玥","女","207230101"};
 
+    printf("Print the firts struct.\n");
     printf("stu1:name=%s,sex=%s,id=%s\n",std1.name,std1.sex,std1.id);
     printf("stu2:name=%s,sex=%s,id=%s\n",std2.name,std2.sex,std2.id);
 
     swap_struct(&std1,&std2);
-
+    
+    printf("Print the second struct.\n");
     printf("2stu1:name=%s,sex=%s,id=%s\n",std1.name,std1.sex,std1.id);
     printf("2stu2:name=%s,sex=%s,id=%s\n",std2.name,std2.sex,std2.id);
     
