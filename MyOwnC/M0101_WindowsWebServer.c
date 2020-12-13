a6024bd78e5e02865552111fc7c6f8ed7b75298e
HailayLin <hailay@qq.com>
2020-12-13 17:52:45 +0800

    github方法

---
+++ MyOwnC/M0101_WindowsWebServer.c
@@ -0,0 +1,17 @@
+/**
+ * @file M0101_WindowsWebServer.c
+ * @author HailayLin (hailay@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-13
+ * 
+ * @copyright Copyright (c) 2020  Hailay.site & Xihe.ai
+ * 
+ */
+
+#include<WinSock2.h>
+
+int main()
+{
+     
+}
