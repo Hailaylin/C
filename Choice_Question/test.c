a38665f0f646061f93350e5b626e17358ee78b0f
HailayLin <hailay@qq.com>
2020-12-23 20:14:07 +0800

    while(!x!=0) s+=++x;

--- Choice_Question/test.c
+++ Choice_Question/test.c
@@ -13,14 +13,8 @@
 
 int main()
 {
-    //测试int的能否成为判断while循环的数
-    int a = +0; double b = -0;
-    int flag=4;
-    while (flag != 0 && a){
-        flag--;
-        printf("1. Yes!\n");
-    }
-    for (flag = 4; flag >= 0 && b ; flag--) printf("2. Yes!\n");
-    //实践证明int负数整数都表示非0——真
+  int x=0,s=0;
+  while(!x!=0) s+=++x;
+  printf("%d",s);
     return 0;
 }
