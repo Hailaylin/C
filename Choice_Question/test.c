df1b9e6bab356a69bc125e322e56a2ad3eff9f88
HailayLin <hailay@qq.com>
2020-12-23 20:38:35 +0800

    测试逗号运算符x=3*y,x+1;

--- Choice_Question/test.c
+++ Choice_Question/test.c
@@ -13,11 +13,8 @@
 
 int main()
 {
-    int x, y;
-    for(y=1;y<10;){
-        y=((x=3*y,x+1),x-1);
-        printf("%d,%d\n",x,y);
-    }
+    int x=0, y=1;
+    x=3*y,x+1;
     
     printf("END:x=%d,y=%d",x,y);
     return 0;
