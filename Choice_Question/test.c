de0177031f979a12dabf4067efd01135c31d37a8
HailayLin <hailay@qq.com>
2020-12-23 19:22:47 +0800

    while(n++<=2);printf("%d",n);n++

---
+++ Choice_Question/test.c
@@ -0,0 +1,20 @@
+/**
+ * @file test.c
+ * @author HailayLin (hailay@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-23
+ * 
+ * @copyright Copyright (c) 2020  Hailay.site & Xihe.ai
+ * 
+ */
+
+#include<stdio.h>
+
+int main()
+{
+    int n=0;
+    while(n++<=2);
+    printf("%d",n);
+    return 0;
+}
