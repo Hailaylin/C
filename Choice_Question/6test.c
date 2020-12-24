f85a9c8e90a5e3b59dc9bcfdb164573dd3e0710c
HailayLin <hailay@qq.com>
2020-12-24 10:44:22 +0800

    统计单词个数新思路

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,18 +1,15 @@
+//下面程序的功能是从键盘输入一行字符，统计其中有多少个单词，单词之间用空格分隔。请选择填空。
 #include<stdio.h>
-
-void fun()
-{
-    static int a[2][2];
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%d\n", *(*a+i));
-    }
-    
-}
-
 int main()
-{
-    fun();
-    return 0;
+{ char s[80],c1,c2=' ';
+  int i=0,num=0;
+  gets(s);
+  while(s[i]!='\0')
+ { c1=s[i];
+    if(i==0) c2=' ';
+    else c2=s[i-1];
+    if(c1!=' ' && c2==' ') num++;
+     i++;
+  }
+printf("There are %d words.\n",num);
 }
-
