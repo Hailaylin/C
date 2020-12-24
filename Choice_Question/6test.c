be5155beef29dce1921b38e76a98392207eb3ff0
HailayLin <hailay@qq.com>
2020-12-24 11:19:26 +0800

    char a[ ]=”morning”,t;             int i,j=0;             for(i=1;i<7;i++)     if(a[j]<a[i]) j=i;             t=a[j];    a[j]=a[7];

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -10,12 +10,3 @@ int main()
     a[7] = a[j];               //* 0~6 7个元素，装完moring
     puts(a);
 }
-
-#include<stdio.h>
-          main()
-          { char a[ ]=”morning”,t;
-            int i,j=0;
-            for(i=1;i<7;i++)     if(a[j]<a[i]) j=i;
-            t=a[j];    a[j]=a[7];
-            a[7]=a[j];    puts(a );
-           }
