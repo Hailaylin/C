b1fcb834a05e37fc0b97726e11241e44cfb30736
HailayLin <hailay@qq.com>
2020-12-24 19:02:35 +0800

    宏定义 #define ADD(x)  x+x; sum=ADD(m+n)*k;

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
-#define FUDGE(y)    2.84+y
-#define PR(x)         printf("%d",(int)(x))
-#define PRINT1(x)    PR(x);putchar('\n')
-int main() { int x=2; PRINT1(FUDGE(5)*x); }
+#include<stdio.h>
+#define ADD(x)  x+x
+int main()
+{ int m=1,n=2,k=3;
+ int sum=ADD(m+n)*k;
+ printf("sum=%d",sum); }
