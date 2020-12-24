279896dc2310a29ea742d5ccf02a327561f4d718
HailayLin <hailay@qq.com>
2020-12-24 18:53:18 +0800

    宏定义 #define FUDGE(y)    2.84+y int x=2; PRINT1(FUDGE(5)*x);

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,24 +1,5 @@
 #include <stdio.h>
-struct bit
-{
-unsigned a_bit:2;
-unsigned b_bit:2;
-unsigned c_bit:1;
-unsigned d_bit:1;
-unsigned e_bit:2;
-unsigned word:8;
-};
-int main()
-{
-struct bit *p;
-unsigned int modeword;
-printf("Enter the mode word (HEX):");
-scanf("%x",&modeword);
-p=(struct bit *)&modeword;
-printf("\n");
-printf("a_bit: %d\n",p ->a_bit);
-printf("b_bit: %d\n",p ->b_bit);
-printf("c_bit: %d\n",p ->c_bit);
-printf("d_bit: %d\n",p ->d_bit);
-printf("e_bit: %d\n",p ->e_bit);
-}
+#define FUDGE(y)    2.84+y
+#define PR(x)         printf("%d",(int)(x))
+#define PRINT1(x)    PR(x);putchar('\n')
+int main() { int x=2; PRINT1(FUDGE(5)*x); }
