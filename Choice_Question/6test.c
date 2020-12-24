b777c2ad6621e71b6095438cc943716f76ba19f4
HailayLin <hailay@qq.com>
2020-12-24 11:01:25 +0800

    char 的各种赋值方法

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
-{ int a[6],i;
-  for(i=1;i<6;i++)
-  {  a[i]=9*(i-2+4*(i>3))%5;  printf("%2d",a[i]); }
+{
+    char a[5]={"abc"};
+    char b[5]={'a','b','c'};
+    char c[5]="";
+    char d[5]="abcde";
+    puts(a);
+    puts(b);
+    puts(c);
+    puts(d);
+    return 0;
 }
