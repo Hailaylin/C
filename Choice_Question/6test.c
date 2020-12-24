f7eee3ed1b008b827916c923572446825c2b9594
HailayLin <hailay@qq.com>
2020-12-24 11:08:03 +0800

    char c[]="\t\v\\\0will\n";     printf("%d",strlen(c));

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
+#include<string.h>
+
 int main()
 {
-    char a[5]={"abc"};
-    char b[5]={'a','b','c'};
-    char c[5]="";
-    char d[5]="abcde";
-    puts(a);
-    puts(b);
-    puts(c);
-    puts(d);
+char c[]="\t\v\\\0will\n";
+    printf("%d",strlen(c));
     return 0;
 }
