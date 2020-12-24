54f2c8df4087556cc5d9ffdeb745f8449e0d5d07
HailayLin <hailay@qq.com>
2020-12-24 10:55:55 +0800

    计算顺序问题

--- Choice_Question/6test.c
+++ Choice_Question/6test.c
@@ -1,15 +1,6 @@
-//下面程序的功能是从键盘输入一行字符，统计其中有多少个单词，单词之间用空格分隔。请选择填空。
 #include<stdio.h>
 int main()
-{ char s[80],c1,c2=' ';
-  int i=0,num=0;
-  gets(s);
-  while(s[i]!='\0')
- { c1=s[i];
-    if(i==0) c2=' ';
-    else c2=s[i-1];
-    if(c1!=' ' && c2==' ') num++;
-     i++;
-  }
-printf("There are %d words.\n",num);
+{ int a[6],i;
+  for(i=1;i<6;i++)
+  {  a[i]=9*(i-2+4*(i>3))%5;  printf("%2d",a[i]); }
 }
