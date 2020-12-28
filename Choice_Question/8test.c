a0692cb89734661d3e757eb7406a3233a7c7cf51
HailayLin <hailay@qq.com>
2020-12-28 18:37:38 +0800

    char *s; s="China!China!China!";

---
+++ Choice_Question/8test.c
@@ -0,0 +1,20 @@
+/**
+ * @file 8test.c
+ * @author HailayLin (hailay@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-28
+ * 
+ * @copyright Copyright (c) 2020  Hailay.site & Xihe.ai
+ * 
+ */
+
+#include<stdio.h>
+
+int main()
+{
+    char *s; s="china";
+    printf("%s", s);
+    return 0;
+
+}
